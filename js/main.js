2b9d7128a965acccaf75d88e0d0db24e9b5af04d
IvanChapler <ererer4646@gmail.com>
2022-11-21 00:21:39 +0500

    Looking for a solution, a script concept

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let question = prompt("Что бы вы хотели спросить?", '');
+let word = question.indexOf("ть");
+
+function handler () {
+    if (question.includes("ть")) {
+        let testWord = question.slice(word - 12)
+        
+    }
+}
+"А что делать?"
