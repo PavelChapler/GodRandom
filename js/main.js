dc94324de400e8ed88bf90a5b7ac63904c4801f7
IvanChapler <ererer4646@gmail.com>
2022-11-26 20:10:10 +0500

    improve script finding verb-questrion

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 
 let base = document.getElementsByTagName('body')[0];
 let open_modal = document.getElementById("confirm");
-let answer = document.getElementById("answer")
+let answer = document.getElementById("answer");
 
 
 function randomizer () {
@@ -14,22 +14,27 @@ function showAnswer () {
     open_modal.onclick = function() {
         let question = document.getElementById("question").value;
         question = question.toLowerCase();
+        let mainWord = getMainWord();
+        
+        function getMainWord () {
+            let suffix;
+                if (question.includes("ть")) suffix = question.indexOf("ть") + 2;
+                if (question.includes("ся")) suffix = question.indexOf("ся") + 2;
+                if (question.includes("ти")) suffix = question.indexOf("ти") + 2;
+            let stroke = question.slice(0, suffix);
+            let words = stroke.split(" ");
+            return words.pop();
+        }
         
         if (typeof answer.innerText !== undefined) answer.innerText = handler() // Для IE8-
         else answer.textContent = handler() // Для нормальных браузеров
 
         function handler () {
-            if (question.includes("пить")) {
-                return (randomizer()) ? "Пить!" : "Не пить!"
-            } else if (question.includes("бухать")) {
-                return (randomizer()) ? "Бухать!" : "Не бухать!"
-            } else if (question.includes("курить")) {
-                return (randomizer()) ? "Курить!" : "Курить вредно"
-            } else if (question.length <= 4) {
-                return "Что?"
+            if (mainWord.length > 1) {
+                return (randomizer()) ? (mainWord[0].toUpperCase() + mainWord.slice(1) + "!") : (`Не ${mainWord}`)
             } else {
                 return (randomizer()) ? "Да" : "Нет"
-            }   
+            }
         }
 
         base.classList.remove("out");
@@ -38,8 +43,10 @@ function showAnswer () {
 
     window.onclick = function(event) {
         if (event.target == document.getElementById("overlay")) {
+
             base.classList.add("out");
-            window.setTimeout(function () {
+
+            window.setTimeout(function () { // Чтобы анимация закрытия успела проиграться
                 base.classList.remove("modal__active");
             }, 500)
         }
